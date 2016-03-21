c421f16ed50dbd1a792573949cbf24655d1f5ab3
oportocala <oportocala@gmail.com>
2016-03-21 12:21:39 +0100

    Cleanup and tests

--- routes/vote.js
+++ routes/vote.js
@@ -1,17 +1,52 @@
 import express from 'express';
 const router = express.Router();
 
-import * as store from '../src/store';
-import { extractData, sanitizeData, isValidVote, onValid, saveVote, onSave } from '../src/vote';
+import { getContestantBySlug, emailExists, saveVote }  from '../src/store';
+import {
+  extractData,
+  validateDataTypes,
+  validateContentTypes,
+  sanitizeData,
+  isValidContestant,
+  isValidEmail
+} from '../src/vote';
+
+
+function logValid (data) {
+  console.log('Valid vote for', data.slug, 'by', data.email);
+  return data;
+}
+
+function logSave (data) {
+  console.log('Vote saved for', data.slug, ' by ', data.email);
+  return true;
+}
+
+const checkContestant = (data) => new Promise( (resolve, reject) => {
+  getContestantBySlug(data.slug)
+    .then(isValidContestant)
+    .then(() => resolve(data))
+    .catch(reject);
+});
+
+const checkEmail = (data) => new Promise( (resolve, reject) => {
+  emailExists(data.email)
+    .then(isValidEmail)
+    .then(() => resolve(data))
+    .catch(reject);
+});
 
 router.post('/:slug', (req, res) => {
 
   extractData(req)
+    .then(validateDataTypes)
+    .then(validateContentTypes)
     .then(sanitizeData)
-    .then((data) => isValidVote(data, store))
-    .then(onValid)
-    .then((data) => store.saveVote(data))
-    .then(onSave)
+    .then(checkContestant)
+    .then(checkEmail)
+    .then(logValid)
+    .then(saveVote)
+    .then(logSave)
     .then(() => {
       res.json({success: true});
     })
--- test/vote.js
+++ test/vote.js
@@ -4,7 +4,7 @@ import chaiAsPromised from 'chai-as-promised';
 const expect = chai.expect;
 chai.use(chaiAsPromised);
 
-import { extractData, sanitizeData } from '../src/vote';
+import { extractData, sanitizeData, validateDataTypes, validateContentTypes, isValidEmail } from '../src/vote';
 
 describe('Vote functions', function() {
   describe('#extractData', function () {
@@ -40,5 +40,38 @@ describe('Vote functions', function() {
     it('should make everything lower-case', () => {
       expect(sanitizeData({email: ' OPOR$#tocala@gmail.com', slug: '@#$AA' })).to.deep.equal({email: 'oportocala@gmail.com', slug: 'aa'});
     })
+  });
+
+  describe('#validateDataTypes', () => {
+    it('should check both are strings', () => {
+      expect(validateDataTypes({email: 1, slug: '' })).to.be.rejected;
+      expect(validateDataTypes({email: true, slug: 'asd' })).to.be.rejected;
+      expect(validateDataTypes({email: '', slug: 1 })).to.be.rejected;
+      expect(validateDataTypes({email: '', slug: null })).to.be.rejected;
+      expect(validateDataTypes({email: '', slug: '' })).to.eventually.equal({email: '', slug: ''});
+    });
+  });
+
+  describe('#validateContentTypes', () => {
+    it('check email, return promise', () => {
+      expect(validateContentTypes({email: 'oportocala', slug: '' })).to.be.rejected;
+      expect(validateContentTypes({email: 'oportocala@', slug: '' })).to.be.rejected;
+      expect(validateContentTypes({email: 'oportocala@test', slug: '' })).to.be.rejected;
+      expect(validateContentTypes({email: 'oportocala@gmail.com', slug: '' })).to.eventually.equal({email: 'oportocala@gmail.com', slug: '' });
+    });
+  });
+
+  describe('#isValidContestant', () => {
+    it('check contestant, return promise', () => {
+      expect(validateContentTypes({finalist: false})).to.be.rejected;
+      expect(validateContentTypes({finalist: true, name: 'ray'})).to.eventually.equal({finalist: true, name: 'ray'});
+    });
+  });
+
+  describe('#isValidEmail', () => {
+    it('check that email does not exist, return promise', () => {
+      expect(isValidEmail(true)).to.be.rejected;
+      expect(isValidEmail(false)).to.eventually.equal(true);
+    });
   });
 });
